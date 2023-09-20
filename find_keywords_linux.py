c1685146478a66bf9aa89e3c33f4635444d4b935
CaptainCuong <captaincuong1692001@gmail.com>
2023-09-20 21:30:56 +0700

    update find keywords for window-wsl and linux

---
+++ find_keywords_linux.py
@@ -0,0 +1,10 @@
+import os
+
+file_dir = ['./path1', './path2', './path3']
+keyword = ".*word_to_find.*"
+
+for dir_ in file_dir:
+	print('\n','-'*25,dir_,'-'*25,'\n')
+	os.system('grep -worne \'' + keyword + '\' ' + dir_)
+	print('\n','*-'*25,'\n')
+os.system('grep -wone \'' + keyword + '\' ' + '* .*')
--- find_keywords_wsl.py
+++ find_keywords_wsl.py
@@ -1,7 +1,7 @@
 import os
 
 file_dir = ['./path1', './path2', './path3']
-keyword = '".*word_to_find.*"'
+keyword = ".*word_to_find.*"
 
 for dir_ in file_dir:
 	print('\n','-'*25,dir_,'-'*25,'\n')
